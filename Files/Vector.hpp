7519d6c5931fc384071a5cf21f689f3fd37fbc8d
nowl01 <itsabdennacerama@gmail.com>
2021-12-26 18:42:09 +0100

    start

---
+++ Files/Vector.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 16:12:11 by abdait-m          #+#    #+#             */
+/*   Updated: 2021/12/26 17:24:51 by abdait-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+# include <iostream>
+
+namespace	ft
+{
+	template<typename T, typename Alloc = std::allocator<T> >
+	class	Vector{
+		
+		
+	};
+}
+
+#endif
