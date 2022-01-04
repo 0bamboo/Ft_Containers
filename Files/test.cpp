2c4eff7b58b07837475526e6f44d05b24061e7bc
nowl01 <itsabdennacerama@gmail.com>
2022-01-04 18:51:30 +0100

    Vector: reserve fucntion

--- Files/test.cpp
+++ Files/test.cpp
@@ -1,22 +1,37 @@
 // # include <iostream>
+# include <stdexcept>
 # include <vector>
 # include "_iter_.hpp"
 
 int main()
 {
-	using namespace std;
+	// using namespace std;
 
-	vector<int> array_int;
+	// vector<int> array_int;
+	// vector<string> array_str;
+	// vector<char> array_char;
+	// vector<long> vec_long;
 
-	for (int i = 0; i < 5; i++)
-		array_int.push_back(i);
-	typedef vector<int>::iterator _iter;
-	ft::_reverseIter<_iter> _revEnd(array_int.begin());
-	ft::_reverseIter<_iter> _revBeg(array_int.end());
+	// cout << array_int.max_size() << "\n" << array_str.max_size() << "\n" \
+	// << array_char.max_size() <<"\n"<< vec_long.max_size() << endl;
+	// std::cout << numeric_limits<long>::max() << endl;
+  try {
+    // vector throws a length_error if resized above max_size
+    std::vector<int> myvector;
+    myvector.resize(myvector.max_size()+1);
+  }
+  catch (const std::length_error& le) {
+	  std::cerr << "Length error: " << le.what() << '\n';
+  }
+	// for (int i = 0; i < 5; i++)
+	// 	array_int.push_back(i);
+	// typedef vector<int>::iterator _iter;
+	// ft::_reverseIter<_iter> _revEnd(array_int.begin());
+	// ft::_reverseIter<_iter> _revBeg(array_int.end());
 
-	_iter it = _revBeg.base();
-	it--; // we need to decrement first cuz the last element is not defined ....
-	for( ; it != _revEnd.base() ; --it)
-		cout << " " << *it << endl;
+	// _iter it = _revBeg.base();
+	// it--; // we need to decrement first cuz the last element is not defined ....
+	// for( ; it != _revEnd.base() ; --it)
+	// 	cout << " " << *it << endl;
 	return 0;
 }
--- Files/vector.hpp
+++ Files/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 16:12:11 by abdait-m          #+#    #+#             */
-/*   Updated: 2022/01/02 15:20:03 by abdait-m         ###   ########.fr       */
+/*   Updated: 2022/01/04 18:47:45 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "_equal_.hpp"
 # include "_lexicographical_.hpp"
 # include "_trait_types_.hpp"
+# include <stdexcept>
 
 namespace	ft
 {
@@ -36,18 +37,90 @@ namespace	ft
 			typedef	typename		ft::_reverseIter<iterator>			rev_iterator;
 			typedef typename		ft::_reverseIter<c_iterator>		c_rev_iterator;
 			typedef					std::ptrdiff_t						difference_type;
-			typedef					std::size_t							size_type;
+			typedef	typename		allocator_type::size_t							size_type;
 		
 		private:
 			size_type		_size_;
+			size_type		_capacity_;
 			allocator_type	_alloctype_;
 			pointer			_data_;
+
+			// Reallocate:
+			void	_reallocate_(size_type	_newS_)
+			{
+				
+			}
 		
 		public:
 			// Constructors
-			explicit vector(const allocator_type& alloc = allocator_type()) : _size_(0), _alloctype_(alloc), _data_(nullptr) { }
+			explicit vector(const allocator_type& alloc = allocator_type()) : 
+			_size_(0), _capacity_(0),_alloctype_(alloc), _data_(nullptr) { } // default constructor
+			explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()):
+			_size_(n), _capacity_(n)
+			{
+				// reserve
+				// assign	
+			} // fill constructor
+			vector(const vector& obj)
+			{
+				if (this != &obj)
+					*this = obj;
+			} // Copy constructor
+			template<typename _inputIter>
+			vector(_inputIter first, _inputIter last, const allocator_type& alloc = allocator_type())
+			{
+				
+			} // fill constructor using iterators
+
+			// Iterators: { ---------
+			// Normal iterators: 
+			iterator		begin() { return (iterator(_data_)); }
+			c_iterator		begin() const { return (c_iterator(_data_)); }
+
+			iterator		end() { return (iterator(_data_ + this->_size_)); }
+			c_iterator		end() const { return (c_iterator(_data_ + this->_size_)); }
 			
-		
+			// Reverse Iterators:
+			rev_iterator	rbegin() { return (rev_iterator(this->end())); }
+			c_rev_iterator	rbegin() const { return (c_rev_iterator(this->end())); }
+
+			rev_iterator	rend() { return (rev_iterator(this->begin())); }
+			c_rev_iterator	rend() const { return (c_rev_iterator(this->begin())); }
+			// --------- }
+
+			// Size and capacity { --------- :
+			size_type	size() const { return (this->_size_); } // the number of elements in the vector.
+			size_type	capacity() const { return (this->_capacity_); } // the size of allocated storage capacity it can be equal or greater than size.
+			size_type	max_size() const { return (_alloctype_.max_size()); } // Maximum size possible to allocate.
+			
+			bool	empty() const { return (this->_size_ == 0); } // Checks whether the vector is empty or not. 
+			
+			void	reserve(size_type n)
+			{
+				if (n > this->_capacity_)
+				{
+					size_type	_newCap_ = (this->_capacity_ * 2 >= n) ? this->_capacity_ * 2 : n;
+					pointer		_newData_ = this->_alloctype_.allocate(this->_capacity_);
+					for (int i = 0; i < this->_size_; i++)
+						this->_alloctype_.construct(_newData_, this->_data_[i]);
+					this->_alloctype_.deallocate(this->_data_, this->_capacity_);
+					this->_data_ = _newData_;
+				}
+			} // Requests a change in capacity.
+
+			
+			
+			void	resize(size_type n, value_type val = value_type())
+			{
+				if (n < this->_size_)
+				{
+					pointer		_newD_ = this->_alloctype_.allocate(n);
+					for (int i = 0; i < n; i++)
+						_newD_[i] = this->_data_[i];
+					this->_alloctype_.deallocate(this->_data_, this->_capacity_);
+				}
+			} // Resize the container so that it contains n elements. 
+			// --------- }
 	};
 	// END! Vector 
 } // END! ft namespace
