9556a501de5fb31608ee73645a8304baa3c83fc8
nowl01 <itsabdennacerama@gmail.com>
2022-02-14 13:30:40 +0100

    map : rbt : [added iterators]

--- Files/_RBT_.hpp
+++ Files/_RBT_.hpp
@@ -6,7 +6,7 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 16:01:47 by abdait-m          #+#    #+#             */
-/*   Updated: 2022/02/13 16:36:52 by abdait-m         ###   ########.fr       */
+/*   Updated: 2022/02/14 13:19:21 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -164,6 +164,133 @@ namespace ft{
 				}
 			
 	}; // END! TREE ITERATOR .
+	
+	// TREE CONST ITERATOR .
+	template<typename _pairType>
+	class	tree_const_iterator
+	{
+		public:
+			
+			typedef	_pairType										iterator_type;
+			typedef const _pairType*								pointer;
+			typedef const _pairType&								reference;
+			typedef std::bidirectional_iterator_tag					iterator_category;
+			typedef std::ptrdiff_t									difference_type;
+			typedef tree_iterator<_pairType>						_self;
+			typedef	typename tree_node<_pairType>::_nodePtr			_node;
+		
+		private:
+			_node	_curr;
+			
+		public:
+			
+			tree_const_iterator() : _curr() { }
+
+			tree_const_iterator(iterator_type iter) : _curr(iter) { }
+
+			template<typename T>
+			tree_const_iterator(const tree_const_iterator<T>& iter) : _curr(iter.base()) { }
+			
+			template<typename T>
+			tree_const_iterator&	operator = (const tree_const_iterator<T>& iter)
+			{
+				this->_curr = iter.base();
+				return (*this);
+			}
+			
+			~tree_const_iterator() { }
+
+			iterator_type	base() const { return (this->_curr); }
+
+			reference	operator *( ) const { return (this->_curr->_pair); }
+
+			pointer		operator ->() const { return (&(operator *())); }
+
+			// Pre increment :
+			_self	operator++() 
+			{
+				this->_curr = next(this->_curr);
+				return (*this);
+			}
+
+			// Post increment :
+			_self	operator++(int)
+			{
+				tree_const_iterator	_tmp(*this);
+
+				this->_curr = next(this->_curr);
+				return (_tmp);
+			}
+
+			_self	operator--()
+			{
+				this->_curr = prev(this->_curr);
+				return (*this);
+			}
+
+			_self	operator--(int)
+			{
+				tree_const_iterator	_tmp(*this);
+
+				this->_curr = prev(this->_curr);
+				return (_tmp);
+			}
+
+			template<typename It>
+			bool	operator == (const tree_const_iterator<It>& cmp)
+			{
+				return (this->_curr == cmp.base());
+			}
+
+			template<typename It>
+			bool	operator != (const tree_const_iterator<It>& cmp)
+			{
+				return (this->_curr != cmp.base());
+			}
+
+			private:
+
+				_node	prev(_node _currNode)
+				{
+					if (_currNode->_left != nullptr)
+					{
+						while (_currNode->_right != nullptr)
+							_currNode = _currNode->_right;
+						return (_currNode);
+					}
+					else
+					{
+						_node _tmp = _currNode->_parent;
+						while (_tmp != nullptr && _currNode == _tmp->_left)
+						{
+							_currNode = _tmp;
+							_tmp = _tmp->_parent;
+						}
+						return (_tmp);
+					}
+				}
+
+				_node	next(_node _currNode)
+				{
+					if (_currNode->_right != nullptr)
+					{
+						while (_currNode->_left != nullptr)
+							_currNode = _currNode->_left;
+						return (_currNode);
+					}
+					else
+					{
+						iterator_type	_tmp = _currNode->_parent;
+						while (_tmp != nullptr && _currNode == _tmp->_right)
+						{
+							_currNode = _tmp;
+							_tmp = _tmp->_parent;
+						}
+						return (_tmp);
+					}
+				}
+			
+	}; // END! TREE ITERATOR .
 
 
 
@@ -184,12 +311,15 @@ namespace ft{
 			typedef typename	_Alloc::const_reference									_allocConstRef;
 			typedef		_Compare														_valueCompare;
 			typedef		tree_iterator<_nodePtr>											iterator;
+			typedef		tree_const_iterator<_nodePtr>									const_iterator; // tree_iterator<const _nodePtr>
 			typedef		ft::_reverseIter<iterator>										reverse_iterator;
+			typedef		ft::_reverseIter<const_iterator>								const_reverse_iterator;
 			typedef	size_t																size_type;
 			typedef std::ptrdiff_t														difference_type;
 
 		private:
 			_nodePtr		_root_;
+			_nodePtr		_endNode_;
 			allocator_type	_alloc;
 			_valueCompare	_comp;
 			size_type		_size;
@@ -222,6 +352,59 @@ namespace ft{
 					_node = _node->_right;
 				return (_node);
 			}
+
+			// Rotation :
+
+		public:
+
+			// Constructors:
+			
+
+			// Iterators:
+			iterator	begin()
+			{
+				if (this->_root_ != nullptr)
+					return (iterator(_treeMinimum(this->_root_)));
+				return (this->_endNode_);
+			}
+
+			const_iterator	begin() const
+			{
+				if (this->_root_ != nullptr)
+					return (const_iterator(_treeMinimum(this->_root_)));
+				return (this->_endNode_);
+			}
+
+			iterator	end()
+			{
+				return (iterator(this->_endNode_));
+			}
+
+			const_iterator	end() const
+			{
+				return (const_iterator(this->_endNode_));
+			}
+
+			reverse_iterator	rbegin()
+			{
+				return (reverse_iterator(this->end()));
+			}
+
+			const_reverse_iterator	rbegin() const
+			{
+				return (const_reverse_iterator(this->end()));
+			}
+
+			reverse_iterator	rend()
+			{
+				return (reverse_iterator(this->begin()));
+			}
+
+			const_reverse_iterator rend() const
+			{
+				return (const_reverse_iterator(this->begin()));
+			}
+			
 			
 	};
 	
